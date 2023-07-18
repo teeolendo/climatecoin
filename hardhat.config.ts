adb990bd26637e23b7cd592a0968411fe8edb5e1
Tony Olendo <teeolendo@gmail.com>
2023-07-18 08:14:51 +0200

    Added zkEVM Testnet RPC

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,31 @@
-import * as dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv'
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
-dotenv.config();
+dotenv.config()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
   networks: {
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+      url: 'https://rpc-mumbai.maticvigil.com',
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
     matic: {
-      url: "https://rpc.ankr.com/polygon",
+      url: 'https://rpc.ankr.com/polygon',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    zkevm: {
+      url: 'https://rpc.public.zkevm-test.net',
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: process.env.POLYSCAN_API_KEY ?? "",
-      polygon: process.env.POLYSCAN_API_KEY ?? "",
+      polygonMumbai: process.env.POLYSCAN_API_KEY ?? '',
+      polygon: process.env.POLYSCAN_API_KEY ?? '',
     },
   },
-};
+}
 
-export default config;
+export default config
